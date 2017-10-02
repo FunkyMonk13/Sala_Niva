84398bc8d8b002b0344d3eab6de29346a12e36d8
FunkyMonk13 <cse35956@students.puas.gr>
2017-10-02 20:15:26 +0300

    Added Azure DB and emails

--- DownloadPage.aspx.cs
+++ DownloadPage.aspx.cs
@@ -16,17 +16,32 @@ public partial class DownloadPage : System.Web.UI.Page
     {
         Label1.Visible = true;
         var email = MailTextBox.Text;
-        CheckDouble(email);
         SendLink(email);
-    }
-
-    protected void CheckDouble(string email)
-    {
-
     }
 
     protected void SendLink(string email)
     {
+        using (SalaNivaEntities db = new SalaNivaEntities())
+        {
+            db.Database.Connection.ConnectionString = "data source=funkymonk.database.windows.net;initial catalog=SalaNiva;persist security info=True;user id=Stergianos;password=FunkyMonks13;MultipleActiveResultSets=True;App=EntityFramework&quot;";
 
+            var check = db.emails.Where(x => x.email == email);
+            if (check.Any())
+            {
+                foreach (var c in check)
+                {
+                    c.counter++;
+                }
+            }
+            else
+            {
+
+                emails m = new emails();
+                m.email = email;
+                m.counter = 1;
+                db.emails.Add(m);
+            }
+            db.SaveChanges();
+        }
     }
 }
